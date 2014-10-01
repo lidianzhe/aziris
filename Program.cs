4248840f7d8b7dc92a78a763d01b8940acb629fc
lihongjun <lihongjun@aizhetech.com>
2014-10-01 21:15:38 +0800

    创建MDIForm,StartupForm

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace acezen.pacs.client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MDIForm());
         }
     }
 }
--- StartupForm.cs
+++ StartupForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace acezen.pacs.client
 {
-    public partial class Form1 : Form
+    public partial class StartupForm : Form
     {
-        public Form1()
+        public StartupForm()
         {
             InitializeComponent();
         }
