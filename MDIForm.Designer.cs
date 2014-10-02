ef4c9e0a41d4e7358c568a3e373b142238ca9cc5
lihongjun <lihongjun@aizhetech.com>
2014-10-02 16:01:31 +0800

    fix MDIForm

--- MDIForm.Designer.cs
+++ MDIForm.Designer.cs
@@ -3,14 +3,14 @@
     partial class MDIForm
     {
         /// <summary>
-        /// 必需的设计器变量。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清理所有正在使用的资源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +20,11 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要
-        /// 使用代码编辑器修改此方法的内容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 783);
+            this.ClientSize = new System.Drawing.Size(1008, 673);
             this.IsMdiContainer = true;
             this.Name = "MDIForm";
             this.Text = "MDIForm";
@@ -46,4 +46,3 @@
         #endregion
     }
 }
-
---
+++ MDIForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace acezen.pacs.client
+{
+    public partial class MDIForm : Form
+    {
+        public MDIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MDIForm_Load(object sender, EventArgs e)
+        {
+            StartupForm startupForm = new StartupForm();
+            startupForm.MdiParent = this;
+            startupForm.Show();
+        }
+    }
+}
